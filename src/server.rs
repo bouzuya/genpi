843429672c7903e87be9268ef545d956a2b6981f
bouzuya <m@bouzuya.net>
2023-07-22 09:37:38 +0900

    Fix to use PORT env

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,11 @@
-use axum::{http::StatusCode, routing::get, Router};
+use std::{
+    env,
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
+
+use anyhow::Context;
+use axum::{http::StatusCode, routing::get, Router, Server};
 
 use crate::pi::{KanaForm, PI};
 
@@ -14,8 +21,18 @@ pub async fn run_server() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/", get(get_root))
         .route("/healthz", get(|| async { "OK" }));
-    // TODO: use PORT env
-    Ok(axum::Server::bind(&"0.0.0.0:3000".parse()?)
+
+    let port_as_string = env::var("PORT").or_else(|e| match e {
+        env::VarError::NotPresent => Ok("3000".to_owned()),
+        env::VarError::NotUnicode(_) => anyhow::bail!("PORT is not unicode"),
+    })?;
+    let port = u16::from_str(port_as_string.as_str()).context("PORT range is (0..=65535)")?;
+    let socket_addr = SocketAddr::new(
+        IpAddr::from_str("0.0.0.0").expect("0.0.0.0 is valid host"),
+        port,
+    );
+
+    Ok(Server::bind(&socket_addr)
         .serve(app.into_make_service())
         .await?)
 }
