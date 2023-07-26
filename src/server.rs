abc2c8e9c476a1477f2809a0adce9404999b8977
bouzuya <m@bouzuya.net>
2023-07-26 21:57:59 +0900

    Add BASE_PATH feature

--- src/server.rs
+++ src/server.rs
@@ -89,22 +89,30 @@ async fn get_root(
 }
 
 pub async fn run_server() -> anyhow::Result<()> {
+    let config = Config::from_env()?;
+
     let shared_state = AppState {
         female_names: Arc::new(Mutex::new(None)),
         male_names: Arc::new(Mutex::new(None)),
     };
-    let app = Router::new()
+    let router = Router::new()
         .route("/", get(get_root))
-        .route("/healthz", get(|| async { "OK" }))
-        .with_state(shared_state);
+        .route("/healthz", get(|| async { "OK" }));
+    let router = if config.base_path.is_empty() {
+        router
+    } else {
+        Router::new()
+            .route("/", get(get_root))
+            .nest(&config.base_path, router)
+    }
+    .with_state(shared_state);
 
-    let config = Config::from_env()?;
     let socket_addr = SocketAddr::new(
         IpAddr::from_str("0.0.0.0").expect("0.0.0.0 is valid host"),
         config.port,
     );
 
     Ok(Server::bind(&socket_addr)
-        .serve(app.into_make_service())
+        .serve(router.into_make_service())
         .await?)
 }
