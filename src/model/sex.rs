14a2d7a30316efdba31f1d2efc58d4990076e063
bouzuya <m@bouzuya.net>
2023-12-06 06:28:39 +0900

    Add impl Distribution<Sex> for Standard

--- src/model/sex.rs
+++ src/model/sex.rs
@@ -1,6 +1,6 @@
 use rand::{thread_rng, Rng};
 
-#[derive(Clone, Copy, Debug, serde::Serialize)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, serde::Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum Sex {
     Female,
@@ -10,5 +10,29 @@ pub enum Sex {
 impl Sex {
     pub fn gen() -> Self {
         [Self::Female, Self::Male][thread_rng().gen_range(0..1)]
+    }
+}
+
+impl rand::distributions::Distribution<Sex> for rand::distributions::Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> Sex {
+        if rng.gen::<bool>() {
+            Sex::Female
+        } else {
+            Sex::Male
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashSet;
+
+    use super::*;
+
+    #[test]
+    fn test_sample() {
+        let mut rng = rand::thread_rng();
+        let set = (0..100).map(|_| rng.gen::<Sex>()).collect::<HashSet<Sex>>();
+        assert_eq!(set.len(), 2);
     }
 }
